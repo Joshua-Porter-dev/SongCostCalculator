18b98388d4dc442b56d5f6c67de828be4ef928c6
joshua-porter-dev <jporter4117@gmail.com>
2019-08-26 23:13:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print(f"You wanted to buy {SongAmmount} songs, that will cost you ${TotalCost}")
 
 # SongAmmount must be turned into a float because by default, the value returned from an input is a string
 # The print statement utailizes f strings as a way to pass variables directly into a strings
+
+# First project in my intro to programming course!
